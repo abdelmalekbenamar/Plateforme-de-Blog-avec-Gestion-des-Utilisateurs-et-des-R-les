4f7b2e94bb014050a9398e5421c56a222e158746
abdelmalekbenamar <abdelmalek.benamar1990@gmail.com>
2024-12-17 12:03:07 +0100

    creation du menu de navigation

---
+++ index.php
@@ -0,0 +1,44 @@
+<h3?php ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/styles.css">
+    <title>Document</title>
+</head>
+<body>
+
+
+    <nav>
+        <ul>
+            <li><a href="#">Articles</a></li>
+            <li><a href="#">Articles Populaires</a></li>
+
+            <li><a href="#">S'inscrire</a></li>
+            <li><a href="#">Se connecter</a></li>
+            <li><a href="#">Paramètres</a></li>
+        </ul>
+    </nav>
+
+
+    <main>
+        <h1>Liste des articles:</h1>
+
+        <div id="articleContainer">
+
+            <div id="articleCard">
+                <!-- <img src="./images/nature.jpg" alt=""> -->
+                <div class="cardInfo">
+                    <h3 class="articleTitle">Titre de essai</h3>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe, quod officiis. Officiis vitae quidem repellendus optio cumque repudiandae sit exercitationem odio rerum facere dolorum recusandae, provident illo! A, neque at?</p>
+                </div>
+            </div>
+
+        </div>
+    </main>
+
+
+
+</body>
+</html>
